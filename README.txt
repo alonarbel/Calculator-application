bae8c1865c22c72f9dbfff66932a33505b7144bd
alonarbel <101185467+alonarbel@users.noreply.github.com>
2023-02-15 17:47:28 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,12 @@
+###############################################################
+Calculator application
+MADE BY:
+Alon Arbel
+###############################################################
+The code was written on Windows machine
+Before running the app make sure that Live Server is installed properly
+###############################################################
+To run the app press f1 and after type "open with Live Server"
+OR
+press the icon "Go Live"
+###############################################################
---
+++ script.js
@@ -0,0 +1,70 @@
+//Switch to 1 if "=" pressed
+let equal_pressed = 0;
+let button_input = document.querySelectorAll(".input-button");
+let input = document.getElementById("input");
+let equal = document.getElementById("equal");
+let clear = document.getElementById("clear");
+let erase = document.getElementById("erase");
+window.onload = () => {
+  input.value = "";
+};
+//Access each class using forEach
+button_input.forEach((button_class) => {
+  button_class.addEventListener("click", () => {
+    //Restart the input string 
+    if (equal_pressed == 1) {
+      input.value = "";
+      equal_pressed = 0;
+    }
+    //Restart the input after an error
+    if(input.value == "SYNTAX ERROR"){
+        input.value = button_class.value;
+    }
+    else{
+        //Cheking if there is two operators in a row
+        if(validInput(button_class.value)){
+            input.value += button_class.value;
+        }
+        else{
+            input.value = "SYNTAX ERROR";
+        }
+    }
+    });
+});
+//Solve the user's input when clicked on equal sign
+equal.addEventListener("click", () => {
+    equal_pressed = 1;
+    let inp_val = ""
+    inp_val = input.value;
+    try {
+      //Evaluate user's input
+        let solution = eval(inp_val);
+        //Checks the /0 issue
+        if(solution == "Infinity"){
+            input.value = "MATH ERROR";
+        }
+        else{
+        if (Number.isInteger(solution)) {
+            input.value = solution;
+        } else {
+            input.value = solution.toFixed(2);
+        }}} 
+        catch (err) {
+        //If user has entered invalid input
+        alert("Invalid Input");
+        }});
+//Clear Whole Input
+clear.addEventListener("click", () => {
+  input.value = "";
+});
+
+function validInput(val){
+    let operators = ["+", "-", "*", "/","."];
+    let str = "";
+    str = input.value;
+    if(operators.includes(val) && operators.includes(str.slice(-1))){
+            return false;
+        }
+    return true;
+}
+
